c2449bb7a71a8fc091a0b36fa1818a8932ae2f79
Niyaz228 <95543962+Niyaz228@users.noreply.github.com>
2022-11-07 01:00:25 +0300

    Add files via upload

--- scripts/gen.js
+++ scripts/gen.js
@@ -1,37 +1,43 @@
 "use strict";
 
 function generatePassword() {
-
         var length = document.getElementById("flying").value,
             charset = "",
             password = "";
 
-        if(document.getElementById("letters").checked)
-            charset += "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        if(document.getElementById("upper").checked)
+            charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        if(document.getElementById("lower").checked)
+            charset += "abcdefghijklmnopqrstuvwxyz";
         if(document.getElementById("numbers").checked)
             charset += "0123456789";
         if(document.getElementById("symbols").checked)
             charset += "!@#$%^&*()-_=+[]{};:',.<>/?";
 
-        for (var i = 0, n = charset.length; i < length; ++i) {
+        for (var i = 0, n = charset.length; i < length; ++i)
             password += charset.charAt(Math.floor(Math.random() * n));
-        }
 
-        if(password == '')
-            password = "mahmood what the hell";
         return password;
 }
 
-function pasteNewPassword(){
-    document.getElementById('myTextarea').value = generatePassword();
+function pasteNewPassword() {
+    let result = generatePassword();
+
+    if(result != "") {
+        document.getElementById("error").style.display = "none";
+        document.getElementById('myTextarea').value = result;
+    } 
+    else
+        document.getElementById("error").style.display = "block";
 } 
 
 function copy() {
-  var copyText = document.getElementById("myTextarea");
+    var copyText = document.getElementById("myTextarea");
 
-  copyText.select(); 
+    copyText.select();
+    document.execCommand("copy");
 
-  document.execCommand("copy");
+    document.getSelection().removeAllRanges();
 }
 
 pasteNewPassword(); //when page loads

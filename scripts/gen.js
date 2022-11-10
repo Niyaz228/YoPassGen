4c31c36b3fd9bb6ebf1dad7502ab02a64bcef9e7
Niyaz228 <95543962+Niyaz228@users.noreply.github.com>
2022-11-10 22:31:17 +0300

    Add files via upload

--- scripts/gen.js
+++ scripts/gen.js
@@ -1,37 +1,84 @@
 "use strict";
 
+var upperset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    lowerset = "abcdefghijklmnopqrstuvwxyz",
+    numbersset = "0123456789",
+    symbolsset = "!#$%^&*()-_=+[]{};:,<.>/?|" + "'" + '"',
+    length,
+    upper,
+    lower,
+    numbers,
+    symbols;
+
 function generatePassword() {
+    length = document.getElementById("flying").value; //getting parameters
+    upper = document.getElementById("upper").checked;
+    lower = document.getElementById("lower").checked;
+    numbers = document.getElementById("numbers").checked;
+    symbols = document.getElementById("symbols").checked;
 
-        var length = document.getElementById("flying").value,
-            charset = "",
-            password = "";
+    var charset = "", 
+        password = "";
 
-        if(document.getElementById("letters").checked)
-            charset += "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        if(document.getElementById("numbers").checked)
-            charset += "0123456789";
-        if(document.getElementById("symbols").checked)
-            charset += "!@#$%^&*()-_=+[]{};:',.<>/?";
-
-        for (var i = 0, n = charset.length; i < length; ++i) {
-            password += charset.charAt(Math.floor(Math.random() * n));
-        }
-
-        if(password == '')
-            password = "mahmood what the hell";
-        return password;
+    if(upper)
+        charset += upperset; //creating charset
+    if(lower)
+        charset += lowerset;
+    if(numbers)
+        charset += numbersset;
+    if(symbols)
+        charset += symbolsset;
+    
+    var n = charset.length; //check whether to show error
+    if(n == 0)
+        return false;
+    
+    for (var i = 0; i < length; ++i) 
+        password += charset.charAt(Math.floor(Math.random() * n));
+        
+    return password;
 }
 
-function pasteNewPassword(){
-    document.getElementById('myTextarea').value = generatePassword();
-} 
+function pasteNewPassword() {
+    var result = generatePassword();
+
+    if(!result) {
+        document.getElementById("error").style.visibility = "visible"; //showing error
+        return;
+    }
+
+    var breakpoint = false;
+    while(!breakpoint) {    //checking whether all parameters are fullfilled
+
+        var isupper = false,
+            islower = false,
+            isnums = false,
+            issymbols = false;
+
+        for (var i = 0; i < length; ++i) {
+            if(upper && upperset.includes(result.charAt(i))) isupper = true;
+            if(lower && lowerset.includes(result.charAt(i))) islower = true;
+            if(numbers && numbersset.includes(result.charAt(i))) isnums = true;
+            if(symbols && symbolsset.includes(result.charAt(i))) issymbols = true;
+        }
+
+        if(upper == isupper && lower == islower && numbers == isnums && symbols == issymbols)
+            breakpoint = true;
+        else
+            result = generatePassword();
+    }
+
+    document.getElementById("error").style.visibility = "hidden"; //giving out password(& hiding error?)
+    document.getElementById("myTextarea").value = result;
+}
 
 function copy() {
-  var copyText = document.getElementById("myTextarea");
+    var copyText = document.getElementById("myTextarea");
 
-  copyText.select(); 
+    copyText.select();
+    document.execCommand("copy");
 
-  document.execCommand("copy");
+    document.getSelection().removeAllRanges();
 }
 
 pasteNewPassword(); //when page loads
